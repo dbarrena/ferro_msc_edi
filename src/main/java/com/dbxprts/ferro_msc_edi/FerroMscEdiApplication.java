1fbc18fbdf82e0a7a8b2f0ce2d412ef2bfa53ede
dbarrera <dbarrena@expedia.com>
2022-02-04 12:13:31 -0600

    enable scheduling

--- src/main/java/com/dbxprts/ferro_msc_edi/FerroMscEdiApplication.java
+++ src/main/java/com/dbxprts/ferro_msc_edi/FerroMscEdiApplication.java
@@ -2,8 +2,10 @@ package com.dbxprts.ferro_msc_edi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FerroMscEdiApplication {
 
 	public static void main(String[] args) {
